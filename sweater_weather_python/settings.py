9bc9f230e309d26de680613d1da451879a8b3a45
Derek Borski <derek.borski@gmail.com>
2020-10-16 13:00:48 -0600

    Update settings for postgres database. Delete unnecessary db.sqlite3 file

--- sweater_weather_python/settings.py
+++ sweater_weather_python/settings.py
@@ -78,10 +78,13 @@ WSGI_APPLICATION = 'sweater_weather_python.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'sweater_weather_python',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
+
 
 
 # Password validation
