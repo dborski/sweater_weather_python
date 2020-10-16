4525942d92cc9b294c92a832601f274555735c51
Derek Borski <derek.borski@gmail.com>
2020-10-16 17:36:09 -0600

    Add spec for photo_service to obtain all photos by keyword search parameter

---
+++ tests/services/test_photo_service.py
@@ -0,0 +1,24 @@
+import os
+import json
+import requests
+from django.test import TestCase
+from api.services.photo_service import get_photos_by_keyword, get_single_photo_by_keyword
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
+
+
+class WeatherService(TestCase):
+  def setUp(self):
+    search_query = 'denver'
+
+    self.results = get_photos_by_keyword(search_query).json()
+    self.single_result = get_single_photo_by_keyword(search_query).json()
+
+  def test_get_photos_by_keyword(self):
+    self.assertIsInstance(self.results['results'], list)
+    self.assertIsNotNone(self.results['results'][0]['width'])
+    self.assertIsNotNone(self.results['results'][0]['height'])
+    self.assertIsNotNone(self.results['results'][0]['urls'])
+    self.assertIsNotNone(self.results['results'][0]['links'])
+    self.assertIsNotNone(self.results['results'][0]['user'])
+
