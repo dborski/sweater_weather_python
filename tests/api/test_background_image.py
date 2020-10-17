cd0beb6c9aa92fa69d31d1208df778cb186849f3
Derek Borski <derek.borski@gmail.com>
2020-10-17 11:40:42 -0600

    Now that a response is being returned, uncomment out to_json() conversion and all tests now passing

--- tests/api/test_background_image.py
+++ tests/api/test_background_image.py
@@ -6,7 +6,7 @@ class GetBackgroundImageTest(TestCase):
   def test_get_background_image_for_city(self):
     response = self.client.get('/api/v1/backgrounds?location=san diego,ca')
 
-    # json_response = response.json()
+    json_response = response.json()
 
     self.assertEqual(response.status_code, 200)
     self.assertEqual(json_response['data']['type'], 'image')
@@ -17,19 +17,3 @@ class GetBackgroundImageTest(TestCase):
     self.assertIsNotNone(json_response['data']['image']['credit']['author'])
     self.assertIsNotNone(json_response['data']['image']['credit']['logo'])
 
-
-# {
-#     "data": {
-#         "type": "image",
-#         "id": null,
-#         "image": {
-#             "location": "denver,co",
-#             "image_url": "https://pixabay.com/get/54e6d4444f50a814f1dc8460962930761c38d6ed534c704c7c2878dd954dc451_640.jpg",
-#             "credit": {
-#                 "source": "pixabay.com",
-#                 "author": "quinntheislander",
-#                 "logo": "https://pixabay.com/static/img/logo_square.png"
-#             }
-#         }
-#     }
-# }
