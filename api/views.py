b149deaf542cceb5ff236a754c390319b3714abb
Derek Borski <derek.borski@gmail.com>
2020-10-16 20:36:56 -0600

    Begin get forecast view by getting forecast for location. Pseudocode out rest of steps that will be done in the ForecastParser

--- api/views.py
+++ api/views.py
@@ -7,6 +7,10 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.parsers import JSONParser
 
+from api.services.location_service import get_latlng
+from api.services.weather_service import get_forecast
+
+
 class ForecastView(APIView):
   parser_classes = [JSONParser]
 
@@ -14,15 +18,22 @@ class ForecastView(APIView):
     # Capture location of requested city
     location = request.GET['location']
 
-    # Split the location into state, city
+    # Split the location into city, state
+    city, state = location.split(",")
 
     # get geocded lat and lng for the above locations
+    results = get_latlng(city, state)
 
     # with lat and lng, get full forecast for city
+    forecast = get_forecast(results['lat'], results['lng']).json()
 
     # Parse through full forecast to pull out only needed attributes
-
+    # ForecastParser(forecast).forecast_payload
     # Setup forecast payload with blank hash to be filled in
-
     # Each part of the hash is filled in by the parser to complete payload
-    return "Hello"
+
+    # import code; code.interact(local=dict(globals(), **locals()))
+
+    # return forecast_payload
+
+    return ""
