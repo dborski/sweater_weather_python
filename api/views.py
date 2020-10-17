028253745c4137f1bad4ed33216f6d75cdaf8e91
Derek Borski <derek.borski@gmail.com>
2020-10-17 10:59:07 -0600

    Create BackgroundView class and get function to handle all requests to api/v1/backgrounds

--- api/views.py
+++ api/views.py
@@ -1,14 +1,11 @@
 import json
 from django.http import JsonResponse, QueryDict
 from django.http import HttpResponse
-from django.shortcuts import render
-from rest_framework import generics
 from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework.parsers import JSONParser
 
 from api.services.location_service import get_latlng
 from api.services.weather_service import get_forecast
+from api.services.photo_service import get_single_photo_by_keyword
 from api.popos.forecast_parser import ForecastParser
 
 
@@ -31,3 +28,23 @@ class ForecastView(APIView):
     forecast_payload = ForecastParser(forecast).get_forecast_payload()
 
     return JsonResponse(forecast_payload)
+
+class BackgroundView(APIView):
+
+  def get(self, request):
+    location = request.GET['location']
+
+    # split string and just use the city
+
+    city = location.split(",")[0]
+
+    # make api call to photo service with get single photo function
+
+    photo_data = get_single_photo_by_keyword(city)
+
+    # Grab information from api response and craft payload in PhotoParser
+    # payload = PhotoParser(photo_data).get_photo_payload()
+
+    # return payload
+
+    import code; code.interact(local=dict(globals(), **locals()))
