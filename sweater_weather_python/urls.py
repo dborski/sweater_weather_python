ed0f3b4999105c89e2f54cc21eabf340d243836e
Derek Borski <derek.borski@gmail.com>
2020-10-16 19:34:33 -0600

    Create new endpoint: api/v1/forecast that routes to ForecastView

--- sweater_weather_python/urls.py
+++ sweater_weather_python/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from api import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/forecast', views.ForecastView.as_view()),
 ]
