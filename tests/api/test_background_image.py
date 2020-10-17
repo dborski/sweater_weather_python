a6ba9e223ed51407c47db4d3ad10ab47039ee803
Derek Borski <derek.borski@gmail.com>
2020-10-17 10:37:46 -0600

    Refactor some specs to account for values sometimes being an integer or float

--- tests/api/test_background_image.py
+++ tests/api/test_background_image.py
@@ -6,7 +6,7 @@ class GetBackgroundImageTest(TestCase):
   def test_get_background_image_for_city(self):
     response = self.client.get('/api/v1/backgrounds?location=san diego,ca')
 
-    json_response = response.json()
+    # json_response = response.json()
 
     self.assertEqual(response.status_code, 200)
     self.assertEqual(json_response['data']['type'], 'image')
