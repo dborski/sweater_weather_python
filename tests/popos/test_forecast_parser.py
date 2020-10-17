77fcfd07ada8ad45b83c7a695122aae97966710f
Derek Borski <derek.borski@gmail.com>
2020-10-17 01:09:48 -0600

    Add specs for parse_daily_weather and the final get_forecast_payload function which returns the entire forecast payload requested

--- tests/popos/test_forecast_parser.py
+++ tests/popos/test_forecast_parser.py
@@ -42,3 +42,21 @@ class ForecastParserTest(TestCase):
     self.assertIsInstance(self.parser.parse_hourly_weather()[0]['conditions'], str)
     self.assertIsInstance(self.parser.parse_hourly_weather()[0]['icon'], str)
 
+  def test_parse_daily_weather(self):
+    self.assertEqual(len(self.parser.parse_daily_weather()), 5)
+    self.assertIsInstance(self.parser.parse_daily_weather()[0]['date'], str)
+    self.assertIsInstance(self.parser.parse_daily_weather()[0]['sunrise'], str)
+    self.assertIsInstance(self.parser.parse_daily_weather()[0]['sunset'], str)
+    self.assertIsInstance(self.parser.parse_daily_weather()[0]['max_temp'], float)
+    self.assertIsInstance(self.parser.parse_daily_weather()[0]['min_temp'], float)
+    self.assertIsInstance(self.parser.parse_daily_weather()[0]['conditions'], str)
+    self.assertIsInstance(self.parser.parse_daily_weather()[0]['icon'], str)
+
+  def test_get_forecast_payload(self):
+    self.assertIsNone(self.parser.get_forecast_payload()['data']['id'])
+    self.assertEqual(self.parser.get_forecast_payload()['data']['type'], 'forecast')
+    self.assertIsInstance(self.parser.get_forecast_payload()['data']['attributes']['current_weather']['datetime'], str)
+    self.assertIsInstance(self.parser.get_forecast_payload()['data']['attributes']['hourly_weather'][0]['time'], str)
+    self.assertIsInstance(self.parser.get_forecast_payload()['data']['attributes']['daily_weather'][0]['date'], str)
+
+
