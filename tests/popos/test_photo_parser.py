a14947c0e2a5cf5e13f33c790593f4089b10d073
Derek Borski <derek.borski@gmail.com>
2020-10-17 11:35:14 -0600

    Add specs for get_credit_info and get_background_payload functions

--- tests/popos/test_photo_parser.py
+++ tests/popos/test_photo_parser.py
@@ -17,3 +17,18 @@ class PhotoParserTest(TestCase):
     self.assertIsInstance(self.parser.image_url, str)
     self.assertIsNotNone(self.parser.user_data)
     self.assertIsInstance(self.parser.location, str)
+  
+  def test_credit_info(self):
+    self.assertEqual(self.parser.get_credit_info()['source'], 'unsplash.com')
+    self.assertIsInstance(self.parser.get_credit_info()['author'], str)
+    self.assertIsInstance(self.parser.get_credit_info()['logo'], str)
+  
+  def test_get_background_payload(self):
+    self.assertEqual(self.parser.get_photo_payload()['data']['type'], 'image')
+    self.assertIsNone(self.parser.get_photo_payload()['data']['id'])
+    self.assertIsInstance(self.parser.get_photo_payload()['data']['image']['location'], str)
+    self.assertIsInstance(self.parser.get_photo_payload()['data']['image']['image_url'], str)
+    self.assertIsInstance(self.parser.get_photo_payload()['data']['image']['credit']['source'], str)
+    self.assertIsInstance(self.parser.get_photo_payload()['data']['image']['credit']['author'], str)
+    self.assertIsInstance(self.parser.get_photo_payload()['data']['image']['credit']['logo'], str)
+
