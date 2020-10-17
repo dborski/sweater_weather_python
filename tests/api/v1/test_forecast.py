4c8ee6eee9dee82c74cd769577da6bf5b67bd46c
Derek Borski <derek.borski@gmail.com>
2020-10-17 01:41:45 -0600

    Update spec for get_weather_for_a_city

--- tests/api/v1/test_forecast.py
+++ tests/api/v1/test_forecast.py
@@ -10,4 +10,9 @@ class GetForecastTest(TestCase):
   def test_get_weather_for_a_city(self):
     response = self.client.get('/api/v1/forecast?location=denver,co')
 
+    json_response = response.json()
+
     self.assertEqual(response.status_code, 200)
+    self.assertIsNotNone(json_response['data']['attributes']['current_weather'])
+    self.assertIsNotNone(json_response['data']['attributes']['hourly_weather'])
+    self.assertIsNotNone(json_response['data']['attributes']['daily_weather'])
