de9a437c8d6e8d1772dbdd10f450182627c5633e
Derek Borski <derek.borski@gmail.com>
2020-10-16 23:27:19 -0600

    Flesh out spec considerably and have complete spec for parse_current_weather function

--- tests/popos/test_forecast_parser.py
+++ tests/popos/test_forecast_parser.py
@@ -1,28 +1,35 @@
 from django.test import TestCase
-from api.popos import ForecastParser
+from api.popos.forecast_parser import ForecastParser
+from api.services.weather_service import get_forecast
+
 
 class ForecastParserTest(TestCase):
   def setUp(self):
-    payload = {"lat": 39.74,
-               "lon": -104.98,
-               "timezone": "America/Denver",
-               "timezone_offset": -21600,
-               "current": {},
-               "hourly": {},
-               "daily": {}
-    }
+    self.lat = '39.738453'
+    self.lng = '-104.984853'
+    self.exclude = 'minutely'
 
-    self.parser = ForecastParser(payload)
+    self.results = get_forecast(self.lat, self.lng, self.exclude).json()
+
+    self.parser = ForecastParser(self.results)
 
   def test_it_exists(self):
     self.assertIsInstance(self.parser, ForecastParser)
 
-  def test_attributse(self):
+  def test_attributes(self):
     self.assertIsInstance(self.parser.current, dict)
-    self.assertIsInstance(self.parser.hourly, dict)
-    self.assertIsInstance(self.parser.daily, dict)
+    self.assertIsInstance(self.parser.hourly, list)
+    self.assertIsInstance(self.parser.daily, list)
 
-  def test_forecast_payload(self):
-    expected = {}
-    self.assertEqual()
+  def test_parse_current_weather(self):
+    self.assertIsInstance(self.parser.parse_current_weather()['datetime'], str)
+    self.assertIsInstance(self.parser.parse_current_weather()['sunrise'], str)
+    self.assertIsInstance(self.parser.parse_current_weather()['sunset'], str)
+    self.assertIsInstance(self.parser.parse_current_weather()['temperature'], float)
+    self.assertIsInstance(self.parser.parse_current_weather()['feels_like'], float)
+    self.assertIsInstance(self.parser.parse_current_weather()['humidity'], int)
+    self.assertIsNotNone(self.parser.parse_current_weather()['uvi'])
+    self.assertIsInstance(self.parser.parse_current_weather()['visibility'], int)
+    self.assertIsInstance(self.parser.parse_current_weather()['conditions'], str)
+    self.assertIsInstance(self.parser.parse_current_weather()['icon'], str)
 
