e956cad0a4c2cd141851d49fb159c13c65afc4f7
Derek Borski <derek.borski@gmail.com>
2020-10-19 21:24:48 -0600

    Delete manually made user model because django has a built in one already created. Create new Profile model that has one-to-one with user model

--- api/models.py
+++ api/models.py
@@ -1,13 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-
-class User(models.Model):
-  username = models.CharField(max_length=30)
-  email = models.CharField(max_length=30)
-  password = models.CharField(max_length=120)
+class Profile(models.Model):
+  user = models.OneToOneField(User, on_delete=models.CASCADE)
   api_key = models.CharField(max_length=60)
-  created_at = models.DateTimeField(auto_now_add=True)
-  updated_at = models.DateTimeField(auto_now=True)
 
-  def __str__(self):
-    return self.username
