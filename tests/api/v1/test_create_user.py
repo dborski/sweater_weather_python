e43655fbc1cb69dd011e4c20acbe904ad41a349f
Derek Borski <derek.borski@gmail.com>
2020-10-19 18:48:58 -0600

    Add create_user spec

---
+++ tests/api/v1/test_create_user.py
@@ -0,0 +1,20 @@
+import json
+from django.test import TestCase
+
+
+class UserTest(TestCase):
+  def test_happy_path_post_new_user(self):
+    body = {
+      'email': 'newuser@email.com',
+      'password': 'password',
+      'password_confirmation': 'password',
+    }
+    response = self.client.post('/api/v1/users', body)
+
+    # json_response = response.json()
+
+    self.assertEqual(response.status_code, 201)
+    self.assertEqual(json_response['data']['type'], 'users')
+    self.assertIsInstance(json_response['data']['id'], str)
+    self.assertEqual(json_response['data']['attributes']['email'], body['email'])
+    self.assertIsInstance(json_response['data']['attributes']['api_key'], str)
