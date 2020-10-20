12b4eb21d0212c8f118a8ba5f91e90577857cf8d
Derek Borski <derek.borski@gmail.com>
2020-10-19 20:26:49 -0600

    Add sad path test for creating user with repeat email

--- tests/api/v1/test_create_user.py
+++ tests/api/v1/test_create_user.py
@@ -5,6 +5,13 @@ from django.contrib.auth.models import User
 
 
 class UserTest(TestCase):
+  def setUp(self):
+    self.user1 = User.objects.create_user(
+      username='currentuser@email.com',
+      email='currentuser@email.com',
+      password='password'
+    )
+
   def test_happy_path_post_new_user(self):
     body = {
       'email': 'newuser@email.com',
@@ -20,3 +27,19 @@ class UserTest(TestCase):
     self.assertIsInstance(json_response['data']['id'], int)
     self.assertEqual(json_response['data']['attributes']['email'], body['email'])
     self.assertIsInstance(json_response['data']['attributes']['api_key'], str)
+
+  def test_sad_path_post_user_with_repeat_email(self):
+    body = {
+      'email': 'currentuser@email.com',
+      'password': 'password',
+      'password_confirmation': 'password',
+    }
+    response = self.client.post('/api/v1/users', body)
+
+    json_response = response.json()
+
+    self.assertEqual(response.status_code, 400)
+    self.assertEqual(json_response['success'], False)
+    self.assertEqual(json_response['error'], 400)
+    self.assertEqual(json_response['errors'], 'This email already exists')
+
