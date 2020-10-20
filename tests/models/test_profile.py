86207cacb7edd9103e856c6927ab37d11a83942e
Derek Borski <derek.borski@gmail.com>
2020-10-19 21:38:45 -0600

    Update profile specs

--- tests/models/test_profile.py
+++ tests/models/test_profile.py
@@ -5,9 +5,12 @@ from api.models import Profile
 class ProfileModelTest(TestCase):
   def setUp(self):
     self.user1 = User.objects.create_user(username='user1@email.com', email='user1@email.com', password='password')
-    self.profile1 = Profile.objects.create(user=self.user1.id, api_key="1234-5678")
+    self.profile1 = self.user1.profile
 
   def test_string_representation(self):
-    self.assertEqual(self.profile1.api_key, "1234-5678")
+    self.assertIsInstance(self.profile1.api_key, str)
+
+  def test_relationship_user_and_profile(self):
+    self.assertIsInstance(self.user1.profile.api_key, str)
 
 
