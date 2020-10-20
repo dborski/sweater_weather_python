c6849611f2534b5bcf0d8d6f7f63ce631f529ba2
Derek Borski <derek.borski@gmail.com>
2020-10-19 20:01:00 -0600

    Add api_key column to users table

--- api/models.py
+++ api/models.py
@@ -5,6 +5,7 @@ class User(models.Model):
   username = models.CharField(max_length=30)
   email = models.CharField(max_length=30)
   password = models.CharField(max_length=120)
+  api_key = models.CharField(max_length=60)
   created_at = models.DateTimeField(auto_now_add=True)
   updated_at = models.DateTimeField(auto_now=True)
 
