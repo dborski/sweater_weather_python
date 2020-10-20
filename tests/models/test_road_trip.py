b87e56155aca5628d08e455f5770a5ca697c7ea6
Derek Borski <derek.borski@gmail.com>
2020-10-20 01:05:24 -0600

    Create RoadTrip model and migrations. Also added created_at and updated_at to Profile model

---
+++ tests/models/test_road_trip.py
@@ -0,0 +1,21 @@
+from django.test import TestCase
+from django.contrib.auth.models import User
+from api.models import Profile
+
+
+class RoadTripModelTest(TestCase):
+  def setUp(self):
+    self.user1 = User.objects.create_user(
+      username='user1@email.com', email='user1@email.com', password='password'
+      )
+    self.road_trip1 = self.user1.roadtrip_set.create(
+      name='road trip 1',
+      start_city='Denver,CO',
+      end_city='Taos,NM',
+      travel_time='4 hours 24 minutes',
+      arrival_temp=76.54,
+      arrival_conditions='mostly cloudy',
+    )
+
+  def test_string_representation(self):
+    self.assertEqual(str(self.road_trip1), self.road_trip1.name)
