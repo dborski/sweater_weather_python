6934e462a74b032c0a5a5fd300655189db7cc0ad
Derek Borski <derek.borski@gmail.com>
2020-10-20 20:00:35 -0600

    Add a multitude of sad path tests for road trip endpoint

--- tests/api/v1/test_road_trip.py
+++ tests/api/v1/test_road_trip.py
@@ -27,3 +27,84 @@ class GetRoadTripTest(TestCase):
     self.assertIsNotNone(json_response['data']['attributes']['travel_time'])
     self.assertIsNotNone(json_response['data']['attributes']['weather_at_eta']['temperature'])
     self.assertIsInstance(json_response['data']['attributes']['weather_at_eta']['conditions'], str)
+
+    def test_sad_path_api_key_missing(self):
+      body = {
+          "origin": "Denver,CO",
+          "destination": "Taos,NM",
+      }
+
+      response = self.client.post('/api/v1/road_trip', body)
+
+      json_response = response.json()
+
+      self.assertEqual(response.status_code, 400)
+      self.assertEqual(json_response['success'], False)
+      self.assertEqual(json_response['error'], 400)
+      self.assertEqual(json_response['errors'], "Must include API key")
+
+    def test_sad_path_origin_missing(self):
+      body = {
+          "destination": "Taos,NM",
+          "api_key": self.user1.profile.api_key
+      }
+
+      response = self.client.post('/api/v1/road_trip', body)
+
+      json_response = response.json()
+
+      self.assertEqual(response.status_code, 400)
+      self.assertEqual(json_response['success'], False)
+      self.assertEqual(json_response['error'], 400)
+      self.assertEqual(json_response['errors'], "Must include origin")
+
+    def test_sad_path_destination_missing(self):
+      body = {
+          "origin": "Denver,CO",
+          "api_key": self.user1.profile.api_key
+      }
+
+      response = self.client.post('/api/v1/road_trip', body)
+
+      json_response = response.json()
+
+      self.assertEqual(response.status_code, 400)
+      self.assertEqual(json_response['success'], False)
+      self.assertEqual(json_response['error'], 400)
+      self.assertEqual(json_response['errors'], "Must include destination")
+
+    def test_sad_path_impossible_route(self):
+      body = {
+          "origin": "Denver,CO",
+          "destination": "London,UK",
+          "api_key": self.user1.profile.api_key
+      }
+
+      response = self.client.post('/api/v1/road_trip', body)
+
+      json_response = response.json()
+
+      self.assertEqual(response.status_code, 400)
+      self.assertIsNone(json_response['data']['id'])
+      self.assertEqual(json_response['data']['attributes']['start_city'], body['origin'])
+      self.assertEqual(json_response['data']['attributes']['end_city'], body['destination'])
+      self.assertEqual(json_response['data']['attributes']['travel_time'], 'Impossible')
+      self.assertIsNone(json_response['data']['attributes']['weather_at_eta'])
+
+    def test_sad_path_trip_longer_than_48_hours(self):
+      body = {
+          "origin": "Denver,CO",
+          "destination": "Managua,Nicaragua",
+          "api_key": self.user1.profile.api_key
+      }
+
+      response = self.client.post('/api/v1/road_trip', body)
+
+      json_response = response.json()
+
+      self.assertEqual(response.status_code, 400)
+      self.assertIsNone(json_response['data']['id'])
+      self.assertEqual(json_response['data']['attributes']['start_city'], body['origin'])
+      self.assertEqual(json_response['data']['attributes']['end_city'], body['destination'])
+      self.assertEqual(json_response['data']['attributes']['travel_time'], 'Impossible')
+      self.assertIsNone(json_response['data']['attributes']['weather_at_eta'])
