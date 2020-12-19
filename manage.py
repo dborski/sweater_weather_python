e98bebb631a429789c5c0c7d1f6bed89d26f61ad
Derek Borski <derek.borski@gmail.com>
2020-12-19 14:31:58 -0700

    print environ for testing

--- manage.py
+++ manage.py
@@ -6,6 +6,7 @@ import sys
 
 def main():
     """Run administrative tasks."""
+    print(os.environ)
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'sweater_weather_python.settings.production')
     try:
         from django.core.management import execute_from_command_line
